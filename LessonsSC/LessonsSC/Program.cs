29187d1781ddf71f365c55a0af50690108b8b1b8
RomAn-8 <komputerrem@gmail.com>
2021-06-24 12:06:57 +0400

    added 33_points_per_level and LessonsSC

---
+++ LessonsSC/LessonsSC/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LessonsSC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Привет мир!");
+
+        }
+    }
+}
