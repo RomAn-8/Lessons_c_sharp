e43d1c03154c2138156bf2c081628b94e28e4859
RomAn-8 <komputerrem@gmail.com>
2021-07-20 10:51:53 +0400

    renamed folders added task

--- LessonsSC/LessonsSC/Program.cs
+++ LessonsSC/LessonsSC/Program.cs
@@ -6,10 +6,16 @@ namespace LessonsSC
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
-            Console.WriteLine("Привет мир!");
-
+            string str;
+            int a, b;
+            Console.WriteLine("Введите число");
+            str = Console.ReadLine();
+            a = Convert.ToInt32(str);
+            Console.WriteLine("Введите число 2");
+            str = Console.ReadLine();
+            b = Convert.ToInt32(str);
+            int result = a + b;
+            Console.WriteLine("Сумма чисел = " + result);
         }
     }
 }
