05b6aa8f09d486bf18cdf9548094711fca269625
RomAn-8 <komputerrem@gmail.com>
2021-08-18 08:45:26 +0400

    added lesson 8_Opertori

---
+++ LessonsSC/LessonsSC/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LessonsSC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            int a, b;
+            Console.WriteLine("Введите число");
+            str = Console.ReadLine();
+            a = Convert.ToInt32(str);
+            Console.WriteLine("Введите число 2");
+            str = Console.ReadLine();
+            b = Convert.ToInt32(str);
+            int result = a + b;
+            Console.WriteLine("Сумма чисел = " + result);
+        }
+    }
+}
