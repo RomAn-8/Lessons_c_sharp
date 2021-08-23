e276c3647ea6675244b76b7082acde229bdbc3d6
RomAn-8 <komputerrem@gmail.com>
2021-08-23 11:09:06 +0400

    added project 1_hw_SredneeArifm

---
+++ LessonsSC/LessonsSC/1_hw_SredneeArifm/1_hw_SredneeArifm/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _1_hw_SredneeArifm
+{
+    /*
+     *  1. Напишите программу вычисляющую среднее арифметическое двух чисел.
+     */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstValue, secondValue;
+
+            Console.WriteLine("Введите число 1");
+
+            firstValue = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите число 2");
+
+            secondValue = double.Parse(Console.ReadLine());
+
+            double result = (firstValue + secondValue) / 2;
+
+            Console.WriteLine("Среднее арифметическое двух чисел = " + result);
+
+        }
+    }
+}
---
+++ sololearn_tasks_c_sharp/7sc_parse_conversion/7sc_parse_conversion/Program.cs
@@ -0,0 +1,43 @@
+﻿
+using System.Globalization;
+ using System;
+
+namespace _7sc_parse_conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "1484";
+            int a;
+            bool result = int.TryParse(str, out a);
+            if (result)
+            {
+                Console.WriteLine("Операция успешна, значение = "+ a);
+            }
+            else
+            {
+                Console.WriteLine("Не удалось конверитровать ");
+               // Console.ReadLine();
+            }
+
+           // try
+         //   {
+          //      int a = Convert.ToInt32(str);
+         //       Console.WriteLine("Успешная конвертация");
+         //   }
+            //catch (Exception)
+           // {
+           //     Console.WriteLine("Ошибка при конвертации");
+           // }
+
+           // double a = double.Parse(str);
+
+
+            // NumberFormatInfo numberFormatInfo = new NumberFormatInfo()
+            // {
+            //     NumberDecimalSeparator = ".",
+            //  };
+        }
+    }
+}
