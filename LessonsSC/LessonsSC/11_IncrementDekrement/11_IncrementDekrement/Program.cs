9cc86c15402f57581ed09580dee763c213376e11
RomAn-8 <komputerrem@gmail.com>
2021-09-24 12:56:11 +0400

    Added 11_InkrementDekremenr

---
+++ LessonsSC/LessonsSC/11_IncrementDekrement/11_IncrementDekrement/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _11_IncrementDekrement
+{//Инкремент и декремент
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 0;
+            int c = 0;
+            int d = 0;
+            int i = 0;
+            int f = 1;
+            a++;
+            b--;
+            ++c;
+            f = ++f * f;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d++);
+            Console.WriteLine(++i);
+            Console.WriteLine(f);
+        }
+    }
+}
